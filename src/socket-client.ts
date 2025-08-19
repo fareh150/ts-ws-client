12b67fa8935c64a760f81d8d2fc1aaac06db9390
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-08-19 18:04:47 +0200

    detectar y mostrar estdo de conexion

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,24 @@
-import { Manager } from "socket.io-client"
+import { Manager, Socket } from "socket.io-client"
 
 export const connectToServer = () =>
 {
     const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
 
     const socket = manager.socket('/');
+
+    addListeners(socket);
+}
+
+const addListeners = (socket: Socket) =>
+{
+    const serverStatusLabel = document.querySelector<HTMLSpanElement>('#server-status')!;
+
+    socket.on('connect', () => {
+        serverStatusLabel.innerText = 'online';
+        
+    })
+
+    socket.on('disconnect', () => {
+        serverStatusLabel.innerText = 'offline';
+    })
 }
