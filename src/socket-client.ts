2cec7e5a5d46998f22bb41046848de2455ed062c
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-08-20 10:28:19 +0200

    limpiar listeners antes de crear uno nuevo

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,5 +1,7 @@
 import { Manager, Socket } from "socket.io-client"
 
+let socket: Socket;
+
 export const connectToServer = (token: string) =>
 {
     const manager = new Manager("http://localhost:3000/socket.io/socket.io.js" ,{
@@ -9,7 +11,10 @@ export const connectToServer = (token: string) =>
         }
     });
 
-    const socket = manager.socket('/');
+    // Limpiar listeners previos
+    socket?.removeAllListeners();
+    
+    socket = manager.socket('/');
 
     addListeners(socket);
 }
