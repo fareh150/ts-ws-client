4442167ca330e7786f9dc4246be5ae7ef2104e77
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-08-20 09:20:29 +0200

    como enviar token a servidor- extraHeaders

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Websockets - Client</h1>
+    <h2>Websockets - Client</h2>
+
+    <input id="jwToken" placeholder="JWT Token" />
+    <button id="connect">Connect</button>
+
+    <br />
 
     <span id="server-status">offline</span>
 
@@ -20,4 +25,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `;
 
-connectToServer();
+//connectToServer();
+
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwToken')!;
+const connectButton = document.querySelector<HTMLButtonElement>('#connect')!;
+
+connectButton.addEventListener('click', () => {
+    if (jwtToken.value.trim().length <= 0) return alert('JWT Token is required');
+
+    connectToServer(jwtToken.value.trim());
+});
